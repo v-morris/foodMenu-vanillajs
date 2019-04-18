b39101a87f2be132b4834cfbc8f340e42aeb29b1
v-morris <digidarkstudio@gmail.com>
2019-04-17 20:24:56 -0400

    Create sorted list in innerHTML when item clicked

--- script.js
+++ script.js
@@ -24,27 +24,27 @@ function setMeal(e) {
     meal.selectedMeal = e.target.id;
     console.log(meal.selectedMeal);
     if (meal.selectedMeal === "breakfast") {
-        document.getElementById('container').innerHTML =
+        document.getElementById('meal-container').innerHTML =
             `<div id="breakfast-container" class="meal-container"> 
             <button id="breakfastBurrito" class="option">Breakfast Burrito</button> 
             <button id="scrambledEggs" class="option">Scrambled Eggs</button>
             <button class="back" onclick="toMeals()">Back to Meals</button></div>`
-        console.log(document.getElementById('container').innerHTML)
+
     } else if (meal.selectedMeal === "lunch") {
-        document.getElementById('container').innerHTML =
+        document.getElementById('meal-container').innerHTML =
             `<div id="lunch-container" class="meal-container">
             <button id="chickenStrips" class="option">Chicken Strips</button>
             <button id="pasta" class="option">Pasta</button>
             <button class="back" onclick="toMeals()">Back to Meals</button></div>`
-        console.log(document.getElementById('container').innerHTML)
+
     } else if (meal.selectedMeal === "dinner") {
-        document.getElementById('container').innerHTML =
+        document.getElementById('meal-container').innerHTML =
             `<div id="dinner-container" class="meal-container">
         <button id="roastBeef" class="option">Roast Beef</button>
         <button id="nyStrip" class="option">NY Strip</button>
         <button class="back" onclick="toMeals()">Back to Meals</button></div>`
     } else if (meal.selectedMeal === "appetizers") {
-        document.getElementById('container').innerHTML =
+        document.getElementById('meal-container').innerHTML =
             `<div id="appetizers-container" class="meal-container">
         <button id="mozzarellaSticks" class="option">Mozzarella Sticks</button>
         <button id="friedZuchinni" class="option">Fried Zuchinni</button>
@@ -57,41 +57,36 @@ function setMeal(e) {
 }
 function itemCount() {
     var item = meal.order;
-    console.log("order", meal.order)
     var counts = {};
     item.forEach(function (i) {
         if (!counts[i]) {
             counts[i] = 0;
             console.log(counts)
-            // li = document.createElement('li');
-            // ul = document.getElementById('order-container');
-            // li.appendChild(document.createTextNode(`${e.target.innerHTML}`));
-            // ul.appendChild(li);
         }
         counts[i] += 1;
     })
+    var itemList = '';
+    for (let item of Object.keys(counts)) {
+        var itemCount = counts[item];
+        ul = document.getElementById('order-container');
+        itemList = itemList + ' ' + (`<li>${item} : ${itemCount}</li>`);
+        ul.innerHTML = itemList;
+    }
     console.log("count", counts);
 }
 
 function addToOrder(e) {
     meal.selectedItems = e.target.innerHTML;
-    // li = document.createElement('li');
-    // ul = document.getElementById('order-container');
-    // li.appendChild(document.createTextNode(`${e.target.innerHTML}`));
-    // ul.appendChild(li);
-    itemCount();
+    itemCount(e);
 }
 
 function toMeals() {
-    document.getElementById('container').innerHTML =
-        `<div id="meal-container" class="meal-container"> 
-        <button id="breakfast" class="option-meal" value="breakfast">Breakfast</button>
+    document.getElementById('meal-container').innerHTML =
+        `<button id="breakfast" class="option-meal" value="breakfast">Breakfast</button>
         <button id="lunch" class="option-meal" value="lunch">Lunch</button>
         <button id="dinner" class="option-meal" value="dinner">Dinner</button>
-        <button id="appetizers" class="option-meal" value="appetizers">Appetizers</button></div>`
-    console.log("meal", document.getElementById('container'))
+        <button id="appetizers" class="option-meal" value="appetizers">Appetizers</button>`
     for (let item of mealSelector) {
         item.addEventListener("click", setMeal);
-        console.log(item);
     }
 }
